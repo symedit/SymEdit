8df3672bc9ab736e2ae83ce6f5d548e9fbc6ef50
Craig Blanchette <craig.blanchette@gmail.com>
2013-06-14 13:50:25 -0400

    Removed unused use statement

--- Entity/Category.php
+++ Entity/Category.php
@@ -3,7 +3,6 @@
 namespace Isometriks\Bundle\SymEditBundle\Entity;
 
 use Isometriks\Bundle\SymEditBundle\Model\Category as AbstractCategory; 
-use Doctrine\Common\Collections\ArrayCollection; 
 
 class Category extends AbstractCategory
 {
