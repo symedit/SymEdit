a11f9f23f77dee7f0d0589b61a619f846625895d
Craig Blanchette <craig.blanchette@gmail.com>
2013-07-12 15:24:26 -0400

    Removed constructor and added a method call to container, much cleaner.

--- Doctrine/UserManager.php
+++ Doctrine/UserManager.php
@@ -3,26 +3,19 @@
 namespace Isometriks\Bundle\SymEditBundle\Doctrine;
 
 use FOS\UserBundle\Doctrine\UserManager as BaseUserManager;
-use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
-use FOS\UserBundle\Util\CanonicalizerInterface;
-use Doctrine\Common\Persistence\ObjectManager;
 
 class UserManager extends BaseUserManager
 {
     protected $profileClass;
     protected $adminProfileClass;
     
-    public function __construct(EncoderFactoryInterface $encoderFactory, 
-                                CanonicalizerInterface $usernameCanonicalizer, 
-                                CanonicalizerInterface $emailCanonicalizer, 
-                                ObjectManager $om, 
-                                $class,
-                                $profileClass,
-                                $adminProfileClass)
+    public function setProfileClass($profileClass)
     {
-        parent::__construct($encoderFactory, $usernameCanonicalizer, $emailCanonicalizer, $om, $class);
-        
         $this->profileClass = $profileClass;
+    }
+    
+    public function setAdminProfileClass($adminProfileClass)
+    {
         $this->adminProfileClass = $adminProfileClass;
     }
     
