e3cf33f5dafe7c33a7e5cfe86dbca11af5f23b67
Craig Blanchette <craig.blanchette@gmail.com>
2013-07-15 19:09:56 -0400

    Switched over the SecurityController to just extend the SymEdit one

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -2,15 +2,8 @@
 
 namespace Isometriks\Bundle\UserBundle\Controller;
 
-use FOS\UserBundle\Controller\SecurityController as BaseController;
+use Isometriks\Bundle\SymEditBundle\Controller\SecurityController as BaseController;
 
 class SecurityController extends BaseController
 {
-    protected function renderLogin(array $data)
-    {
-        return $this->container->get('templating')->renderResponse(
-            'IsometriksSymEditBundle:Security:login.html.twig',
-            $data
-        );
-    }
 }
