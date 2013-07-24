b3dd803be1c3e36af80d39c3736155178f49089f
Craig Blanchette <craig.blanchette@gmail.com>
2013-07-24 11:05:02 -0400

    Added some caching to widget areas. Moved the template finding below the 304 chance to take up less time.

--- Controller/WidgetController.php
+++ Controller/WidgetController.php
@@ -4,22 +4,18 @@ namespace Isometriks\Bundle\SymEditBundle\Controller;
 
 use Symfony\Component\Templating\TemplateReference;
 use Isometriks\Bundle\SymEditBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class WidgetController extends Controller
 {
-    public function renderAreaAction($area, $path = null, $id = null)
+    public function renderAreaAction(Request $request, $area, $path = null, $id = null)
     {
         $manager = $this->get('isometriks_symedit.widget.manager');
         $widgetArea = $manager->getWidgetArea($area);
 
-        $templateName = sprintf('@SymEdit/WidgetArea/%s.html.twig', $area);
-        $template = new TemplateReference($templateName);
-
-        if(!$this->templateExists($template)){
-            $template->set('name', '@SymEdit/WidgetArea/default.html.twig');
-        }
-
         $response = $this->createResponse();
+
+        $lastUpdated = null;
         $widgets = array();
 
         foreach($widgetArea->getWidgets() as $widget){
@@ -28,11 +24,29 @@ class WidgetController extends Controller
                 continue;
             }
 
+            $lastUpdated = max($widget->getUpdatedAt(), $lastUpdated);
+
             $widgets[] = array(
                 'id' => $widget->getId(),
                 'title' => $widget->getTitle(),
                 'content' => $widget->getStrategy()->execute($widget),
             );
+        }
+
+        if($lastUpdated !== null){
+            $response->setLastModified($lastUpdated);
+
+            if($response->isNotModified($request)) {
+                $response->setSharedMaxAge(60);
+                return $response;
+            }
+        }
+
+        $templateName = sprintf('@SymEdit/WidgetArea/%s.html.twig', $area);
+        $template = new TemplateReference($templateName);
+
+        if(!$this->templateExists($template)){
+            $template->set('name', '@SymEdit/WidgetArea/default.html.twig');
         }
 
         return $this->render($template, array(
