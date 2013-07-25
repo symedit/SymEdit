000bda96cd229701c1fe4f1b025b46fadf741cfc
Craig Blanchette <craig.blanchette@gmail.com>
2013-07-25 15:56:53 -0400

    Added a featured widget area, and added the new slider to it as well only on homepage.

--- DataFixtures/ORM/LoadWidgetData.php
+++ DataFixtures/ORM/LoadWidgetData.php
@@ -7,9 +7,9 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
-use Isometriks\Bundle\SymEditBundle\Model\Widget; 
+use Isometriks\Bundle\SymEditBundle\Model\Widget;
 
-class LoadWidgetData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface 
+class LoadWidgetData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
     /**
      * @var ContainerInterface
@@ -26,79 +26,99 @@ class LoadWidgetData extends AbstractFixture implements OrderedFixtureInterface,
         /**
          * Create sidebar
          */
-        $sidebar = $this->getManager()->createWidgetArea(); 
-        $sidebar->setArea('sidebar'); 
-        $sidebar->setDescription('Sidebar Widget Area'); 
-        
+        $sidebar = $this->getManager()->createWidgetArea();
+        $sidebar->setArea('sidebar');
+        $sidebar->setDescription('Sidebar Widget Area');
+
         /**
          * Fetch Blog Page
          */
         $blog = $this->getReference('page-blog');
-        
+
         /**
          * Add Categories to Blog
          */
-        $categories = $this->getManager()->createWidget('blog_categories'); 
+        $categories = $this->getManager()->createWidget('blog_categories');
         $categories->setName('blog_categories')
                    ->setTitle('Blog Categories')
                    ->setVisibility(Widget::INCLUDE_ONLY)
-                   ->addAssoc($blog->getId()); 
-        
+                   ->addAssoc($blog->getId());
+
         /**
-         * Add Recent Posts to Blog 
+         * Add Recent Posts to Blog
          */
-        $recent = $this->getManager()->createWidget('blog_recent_posts'); 
+        $recent = $this->getManager()->createWidget('blog_recent_posts');
         $recent->setName('blog_recent_posts')
                ->setTitle('Recent Posts')
-               ->setVisibility(Widget::INCLUDE_ONLY) 
-               ->addAssoc($blog->getId());  
+               ->setVisibility(Widget::INCLUDE_ONLY)
+               ->addAssoc($blog->getId());
 
         /**
          * Add Widgets to Sidebar
          */
-        $sidebar->addWidget($categories); 
-        $sidebar->addWidget($recent); 
-        
+        $sidebar->addWidget($categories);
+        $sidebar->addWidget($recent);
+
         $this->getManager()->saveWidgetArea($sidebar);
-        
-        $this->addReference('widgetarea-sidebar', $sidebar); 
-        $this->addReference('widget-blog-categories', $categories); 
+
+        $this->addReference('widgetarea-sidebar', $sidebar);
+        $this->addReference('widget-blog-categories', $categories);
         $this->addReference('widget-blog-recent-posts', $recent);
-        
-        
+
+
         /**
          * Add Footer Area
          */
-        $footer = $this->getManager()->createWidgetArea(); 
-        $footer->setArea('footer'); 
-        $footer->setDescription('Footer Widget Area'); 
-        
-        $contact = $this->getManager()->createWidget('contact_info'); 
+        $footer = $this->getManager()->createWidgetArea();
+        $footer->setArea('footer');
+        $footer->setDescription('Footer Widget Area');
+
+        $contact = $this->getManager()->createWidget('contact_info');
         $contact->setName('contact_info')
                 ->setTitle('Contact Information')
-                ->setVisibility(Widget::INCLUDE_ALL); 
-        
-        $about = $this->getManager()->createWidget('html'); 
+                ->setVisibility(Widget::INCLUDE_ALL);
+
+        $about = $this->getManager()->createWidget('html');
         $about->setName('about')
               ->setTitle('About Us')
               ->setVisibility(Widget::INCLUDE_ALL)
-              ->setOption('html', '<p>This is all about our company...</p>'); 
-        
+              ->setOption('html', '<p>This is all about our company...</p>');
+
         /**
          * Add Widgets to footer
          */
-        $footer->addWidget($contact); 
-        $footer->addWidget($about); 
-        
-        $this->getManager()->saveWidgetArea($footer); 
+        $footer->addWidget($contact);
+        $footer->addWidget($about);
+
+        $this->getManager()->saveWidgetArea($footer);
+
+
+        /**
+         * Add Featured Area
+         */
+        $featured = $this->getManager()->createWidgetArea();
+        $featured
+            ->setArea('featured')
+            ->setDescription('Featured Widget Area at top of page');
+
+        $slider = $this->getManager()->createWidget('slider');
+        $slider
+            ->setName('homepage_slider')
+            ->setTitle('Homepage Slider')
+            ->setVisibility(Widget::INCLUDE_ONLY)
+            ->addAssoc('/');
+
+        $featured->addWidget($slider);
+
+        $this->getManager()->saveWidgetArea($featured);
     }
-    
+
     /**
      * @return \Isometriks\Bundle\SymEditBundle\Widget\WidgetManager
      */
     private function getManager()
     {
-        return $this->container->get('isometriks_symedit.widget.manager'); 
+        return $this->container->get('isometriks_symedit.widget.manager');
     }
 
     public function getOrder()
