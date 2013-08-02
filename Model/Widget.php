a77a0b032d023d7b6d1c3a5f5390d1f55c68c1c3
Craig Blanchette <craig.blanchette@gmail.com>
2013-08-02 15:03:19 -0400

    Return null on a getOption that doesn't exist

--- Model/Widget.php
+++ Model/Widget.php
@@ -1,36 +1,36 @@
 <?php
 
-namespace Isometriks\Bundle\SymEditBundle\Model; 
+namespace Isometriks\Bundle\SymEditBundle\Model;
 
-use Isometriks\Bundle\SymEditBundle\Widget\Strategy\WidgetStrategyInterface; 
+use Isometriks\Bundle\SymEditBundle\Widget\Strategy\WidgetStrategyInterface;
 
 abstract class Widget implements WidgetInterface
-{ 
-    const INCLUDE_ALL = 0; 
-    const INCLUDE_ONLY = 1; 
-    const EXCLUDE_ONLY = 2; 
-    
+{
+    const INCLUDE_ALL = 0;
+    const INCLUDE_ONLY = 1;
+    const EXCLUDE_ONLY = 2;
+
     /**
      * @var integer
      */
     protected $id;
-    
+
     /**
      * @var string Widget Name
      */
-    protected $name; 
+    protected $name;
 
     /**
      *
      * @var string Widget Title
      */
-    protected $title; 
-    
+    protected $title;
+
     /**
      * @var WidgetAreaInterface Widget Area
      */
-    protected $area; 
-    
+    protected $area;
+
     /**
      * @var array
      */
@@ -39,113 +39,117 @@ abstract class Widget implements WidgetInterface
     /**
      * @var string
      */
-    protected $strategyName; 
-    
+    protected $strategyName;
+
     /**
      * @var string
      */
-    protected $strategy; 
-    
+    protected $strategy;
+
     /**
-     * @var integer 
+     * @var integer
      */
-    protected $visibility; 
-    
+    protected $visibility;
+
     /**
-     * @var array 
+     * @var array
      */
-    protected $assoc; 
-    
+    protected $assoc;
+
     /**
      * @var \DateTime
      */
-    protected $createdAt; 
-    
+    protected $createdAt;
+
     /**
      * @var \DateTime
-     */    
-    protected $updatedAt; 
+     */
+    protected $updatedAt;
 
     public function __construct()
     {
-        $this->setOptions(array()); 
-        $this->setVisibility(self::INCLUDE_ALL); 
-        $this->setAssoc(array()); 
-        $this->setUpdatedAt(new \DateTime()); 
+        $this->setOptions(array());
+        $this->setVisibility(self::INCLUDE_ALL);
+        $this->setAssoc(array());
+        $this->setUpdatedAt(new \DateTime());
     }
-    
+
     public function getId()
     {
         return $this->id;
     }
-    
+
     public function setName($name)
     {
-        $this->name = $name; 
-        
-        return $this; 
+        $this->name = $name;
+
+        return $this;
     }
-    
+
     public function getName()
     {
-        return $this->name; 
+        return $this->name;
     }
-    
+
     public function setTitle($title)
     {
-        $this->title = $title; 
-        
-        return $this; 
+        $this->title = $title;
+
+        return $this;
     }
-    
+
     public function getTitle()
     {
-        return $this->title; 
+        return $this->title;
     }
-    
+
     public function setArea(WidgetAreaInterface $area)
     {
         $this->area = $area;
     }
-    
+
     public function getArea()
     {
-        return $this->area; 
+        return $this->area;
     }
 
     public function setOptions(array $options)
     {
         $this->options = $options;
-    
+
         return $this;
     }
-    
+
     public function setOption($option, $value)
     {
-        $this->options[$option] = $value; 
-        
-        return $this; 
+        $this->options[$option] = $value;
+
+        return $this;
     }
 
     public function getOptions()
     {
         return $this->options;
     }
-    
+
     public function getOption($option)
     {
-        return $this->options[$option]; 
+        if(!isset($this->options[$option])) {
+            return null;
+        }
+
+        return $this->options[$option];
     }
-    
+
     public function hasOption($option)
     {
-        return isset($this->options[$option]); 
+        return isset($this->options[$option]);
     }
 
     public function setStrategyName($strategyName)
     {
         $this->strategyName = $strategyName;
-    
+
         return $this;
     }
 
@@ -153,76 +157,76 @@ abstract class Widget implements WidgetInterface
     {
         return $this->strategyName;
     }
-    
-    /** 
+
+    /**
      * @return WidgetStrategyInterface
      */
     public function getStrategy()
     {
-        return $this->strategy; 
+        return $this->strategy;
     }
-    
+
     public function setStrategy(WidgetStrategyInterface $strategy)
     {
         $this->strategy = $strategy;
-        $this->strategyName = $strategy->getName(); 
+        $this->strategyName = $strategy->getName();
     }
-    
+
     public function setVisibility($visibility)
     {
-        $this->visibility = $visibility; 
-        
-        return $this; 
+        $this->visibility = $visibility;
+
+        return $this;
     }
-    
+
     public function getVisibility()
     {
-        return $this->visibility; 
+        return $this->visibility;
     }
-    
+
     public function isVisible(array $strings)
     {
         return $this->getVisibility() === self::INCLUDE_ALL ||
               ($this->getVisibility() === self::INCLUDE_ONLY && $this->hasAssoc($strings)) ||
-              ($this->getVisibility() === self::EXCLUDE_ONLY && !$this->hasAssoc($strings)); 
+              ($this->getVisibility() === self::EXCLUDE_ONLY && !$this->hasAssoc($strings));
     }
-    
-    
+
+
     public function setAssoc(array $assoc)
     {
-        $this->assoc = $assoc; 
-        
-        return $this; 
+        $this->assoc = $assoc;
+
+        return $this;
     }
-    
+
     public function addAssoc($assoc)
     {
         $this->assoc[] = $assoc;
-        
-        return $this; 
+
+        return $this;
     }
-    
+
     public function removeAssoc($assoc)
     {
         foreach($this->assoc as $key=>$value){
             if($assoc == $value){
-                unset($this->assoc[$key]); 
+                unset($this->assoc[$key]);
             }
         }
-        
+
         /**
          * Reset array keys
          */
-        $this->assoc = array_values($this->assoc); 
-        
-        return $this; 
+        $this->assoc = array_values($this->assoc);
+
+        return $this;
     }
-    
+
     public function getAssoc()
     {
-        return $this->assoc; 
+        return $this->assoc;
     }
-    
+
     public function hasAssoc(array $strings)
     {
         foreach($strings as $assoc){
@@ -230,56 +234,56 @@ abstract class Widget implements WidgetInterface
                 return true;
             }
         }
-        
+
         return false;
-    }    
-    
+    }
+
     protected function checkAssoc($string)
     {
         if(empty($string)){
             return false;
         }
-        
+
         /**
          * Remove trailing slash
          */
-        $string = rtrim($string, '/'); 
-        
+        $string = rtrim($string, '/');
+
         foreach($this->assoc as $assoc){
-            
-            $assoc = rtrim($assoc, '/'); 
-            
+
+            $assoc = rtrim($assoc, '/');
+
             /**
              * Check for wildcard.
-             * 
+             *
              * Have to replace by \* because preg_quote will add a slash before
-             * the star. 
+             * the star.
              */
             if(strpos($assoc, '*') !== false){
-                
-                $regexp = '#'. str_replace('\*', '.+?', preg_quote($assoc)).'#i'; 
-                
+
+                $regexp = '#'. str_replace('\*', '.+?', preg_quote($assoc)).'#i';
+
                 if(preg_match($regexp, $string)){
-                    return true; 
+                    return true;
                 }
-                
+
             } elseif ($string === $assoc) {
-                
-                return true; 
+
+                return true;
             }
         }
-        
-        return false; 
+
+        return false;
     }
-    
+
     /**
      * @return \DateTime $createdAt
      */
     public function getCreatedAt()
     {
-        return $this->createdAt; 
+        return $this->createdAt;
     }
-    
+
     /**
      * @param \DateTime $updatedAt
      * @return WidgetInterface $widget
@@ -287,15 +291,15 @@ abstract class Widget implements WidgetInterface
     public function setUpdatedAt($updatedAt)
     {
         $this->updatedAt = $updatedAt;
-        
-        return $this; 
+
+        return $this;
     }
-    
+
     /**
      * @return \DateTime $updatedAt
      */
     public function getUpdatedAt()
     {
-        return $this->updatedAt; 
+        return $this->updatedAt;
     }
 }
