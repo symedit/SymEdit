d4247adab92250e79eef5f1130909d4555ef9ad4
Craig Blanchette <craig.blanchette@gmail.com>
2013-08-02 15:03:47 -0400

    Removed hostBundle references, no longer needed

--- Widget/Strategy/ContactInfoStrategy.php
+++ Widget/Strategy/ContactInfoStrategy.php
@@ -1,31 +1,31 @@
 <?php
 
-namespace Isometriks\Bundle\SymEditBundle\Widget\Strategy; 
+namespace Isometriks\Bundle\SymEditBundle\Widget\Strategy;
 
-use Symfony\Component\Form\FormBuilderInterface; 
-use Isometriks\Bundle\SymEditBundle\Model\WidgetInterface; 
+use Symfony\Component\Form\FormBuilderInterface;
+use Isometriks\Bundle\SymEditBundle\Model\WidgetInterface;
 
 class ContactInfoStrategy extends TemplateStrategy
 {
     /**
-     * Return just the regular form, you can't change the template. 
+     * Return just the regular form, you can't change the template.
      */
     public function buildForm(FormBuilderInterface $builder)
     {
     }
-    
+
     public function setDefaultOptions(WidgetInterface $widget)
     {
-        $widget->setOption('template', sprintf('%s:Widget:contact-info.html.twig', $this->getHostBundle())); 
+        $widget->setOption('template', '@SymEdit/Widget/contact-info.html.twig');
     }
-    
+
     public function getName()
     {
-        return 'contact_info'; 
+        return 'contact_info';
     }
-    
+
     public function getDescription()
     {
-        return 'Basic Contact Information'; 
+        return 'Basic Contact Information';
     }
 }
--- Widget/Strategy/TemplateStrategy.php
+++ Widget/Strategy/TemplateStrategy.php
@@ -2,57 +2,50 @@
 
 namespace Isometriks\Bundle\SymEditBundle\Widget\Strategy;
 
-use Symfony\Component\Form\FormBuilderInterface; 
-use Isometriks\Bundle\SymEditBundle\Model\WidgetInterface; 
-use Symfony\Component\Validator\Constraints\NotBlank; 
+use Symfony\Component\Form\FormBuilderInterface;
+use Isometriks\Bundle\SymEditBundle\Model\WidgetInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TemplateStrategy extends AbstractWidgetStrategy
 {
-    private $host_bundle;
-    private $twig; 
-    
-    public function __construct($host_bundle, \Twig_Environment $twig)
+    private $twig;
+
+    public function __construct(\Twig_Environment $twig)
     {
-        $this->host_bundle = $host_bundle;
-        $this->twig = $twig; 
+        $this->twig = $twig;
     }
 
     public function execute(WidgetInterface $widget)
-    {        
+    {
         try {
-            $content = $this->twig->render($widget->getOption('template')); 
+            $content = $this->twig->render($widget->getOption('template'));
         } catch(\Exception $e){
-            $content = sprintf('There was an error rendering your template: "%s"', $e->getMessage()); 
+            $content = sprintf('There was an error rendering your template: "%s"', $e->getMessage());
         }
-        
-        return $content; 
+
+        return $content;
     }
 
     public function buildForm(FormBuilderInterface $builder)
     {
         $builder
             ->add('template', 'text', array(
-                'required' => true, 
-                'label' => 'Template', 
-                'property_path' => 'options[template]', 
+                'required' => true,
+                'label' => 'Template',
+                'property_path' => 'options[template]',
                 'constraints' => array(
-                    new NotBlank(), 
-                ), 
-            ));  
-    }    
-    
-    public function getHostBundle()
-    {
-        return $this->host_bundle; 
+                    new NotBlank(),
+                ),
+            ));
     }
-    
+
     public function getName()
     {
-        return 'template'; 
+        return 'template';
     }
 
     public function getDescription()
     {
-        return 'Template'; 
+        return 'Template';
     }
 }
