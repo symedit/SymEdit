b71828d465af4a618a96023cfa0da0ffee108adc
Craig Blanchette <craig.blanchette@gmail.com>
2013-08-06 13:09:08 -0400

    Removed hostbundle references

--- Widget/Strategy/BlogCategoriesStrategy.php
+++ Widget/Strategy/BlogCategoriesStrategy.php
@@ -2,58 +2,56 @@
 
 namespace Isometriks\Bundle\SymEditBundle\Widget\Strategy;
 
-use Symfony\Component\Form\FormBuilderInterface; 
-use Isometriks\Bundle\SymEditBundle\Model\WidgetInterface; 
-use Doctrine\Bundle\DoctrineBundle\Registry; 
+use Symfony\Component\Form\FormBuilderInterface;
+use Isometriks\Bundle\SymEditBundle\Model\WidgetInterface;
+use Doctrine\Bundle\DoctrineBundle\Registry;
 
 class BlogCategoriesStrategy extends AbstractWidgetStrategy
 {
-    private $twig; 
-    private $doctrine; 
-    private $host_bundle; 
-    
-    public function __construct(\Twig_Environment $twig, Registry $doctrine, $host_bundle)
+    private $twig;
+    private $doctrine;
+
+    public function __construct(\Twig_Environment $twig, Registry $doctrine)
     {
-        $this->twig = $twig; 
-        $this->doctrine = $doctrine; 
-        $this->host_bundle = $host_bundle; 
+        $this->twig = $twig;
+        $this->doctrine = $doctrine;
     }
 
     public function execute(WidgetInterface $widget)
     {
-        $categories = $this->doctrine->getManager()->getRepository('IsometriksSymEditBundle:Category'); 
-        
-        return $this->twig->render($this->host_bundle . ':Widget:blog-categories.html.twig', array(
+        $categories = $this->doctrine->getManager()->getRepository('IsometriksSymEditBundle:Category');
+
+        return $this->twig->render('@SymEdit/Widget/blog-categories.html.twig', array(
             'categories' => $categories,
-            'counts' => $widget->getOption('counts'), 
-        )); 
+            'counts' => $widget->getOption('counts'),
+        ));
     }
 
     public function buildForm(FormBuilderInterface $builder)
     {
         $builder
             ->add('counts', 'checkbox', array(
-                'required' => false, 
-                'label' => 'Display Counts', 
-                'help_block' => 'Display Category counts next to label', 
-                'property_path' => 'options[counts]', 
-            )); 
-    }    
-    
+                'required' => false,
+                'label' => 'Display Counts',
+                'help_block' => 'Display Category counts next to label',
+                'property_path' => 'options[counts]',
+            ));
+    }
+
     public function setDefaultOptions(WidgetInterface $widget)
     {
         $widget->setOptions(array(
-            'counts' => true, 
-        )); 
+            'counts' => true,
+        ));
     }
-    
+
     public function getName()
     {
-        return 'blog_categories'; 
+        return 'blog_categories';
     }
 
     public function getDescription()
     {
-        return 'Blog Categories'; 
+        return 'Blog Categories';
     }
 }
