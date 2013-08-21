90433c79154281a3f20b8f023b8da01eb12bd574
Craig Blanchette <craig.blanchette@gmail.com>
2013-08-21 14:15:20 -0400

    Remove unused class

--- Form/EventListener/RemoveFieldsSubscriber.php
+++
@@ -1,4 +0,0 @@
-
-
-  In this I was going to use it to remove a list of fields passed to the constructor
-    if the object is an actual entity but I remember having problems using ->remove('name')...
