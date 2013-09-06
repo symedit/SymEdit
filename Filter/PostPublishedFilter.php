1b72c2881c770a783459ad98b56e211a9c6c87d7
Craig Blanchette <craig.blanchette@gmail.com>
2013-09-06 13:34:05 -0400

    Changed published filter to use the Model and not the Entity

--- Filter/PostPublishedFilter.php
+++ Filter/PostPublishedFilter.php
@@ -3,16 +3,16 @@
 namespace Isometriks\Bundle\SymEditBundle\Filter;
 
 use Doctrine\ORM\Query\Filter\SQLFilter;
-use Isometriks\Bundle\SymEditBundle\Entity\Post;
-use Doctrine\ORM\Mapping\ClassMetadata; 
+use Isometriks\Bundle\SymEditBundle\Model\Post;
+use Doctrine\ORM\Mapping\ClassMetadata;
 
 class PostPublishedFilter extends SQLFilter
 {
-    private $postClass = 'Isometriks\\Bundle\\SymEditBundle\\Entity\\Post';
+    private $postClass = 'Isometriks\\Bundle\\SymEditBundle\\Model\\Post';
 
     public function addFilterConstraint(ClassMetadata $targetEntity, $targetTableAlias)
     {
-        if ($targetEntity->reflClass->name !== $this->postClass) {
+        if (!$targetEntity->reflClass->isSubclassOf($this->postClass)) {
             return '';
         }
 
