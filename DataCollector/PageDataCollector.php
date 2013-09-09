8a4b6ab5db33585358214d5e281164544884ab43
Craig Blanchette <craig.blanchette@gmail.com>
2013-09-09 15:41:06 -0400

    Cleanup the collector

--- DataCollector/PageDataCollector.php
+++ DataCollector/PageDataCollector.php
@@ -8,33 +8,28 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PageDataCollector extends DataCollector
 {
-    public function __construct()
-    {
-        $this->data = null;
-    }
-
     public function collect(Request $request, Response $response, \Exception $exception = null)
     {
-
         if ($request->attributes->has('_page')) {
-
             $page = $request->attributes->get('_page');
 
             $this->data = array(
-                'id' => $page->getId(),
-                'path' => $page->getPath(),
-                'title' => $page->getTitle(),
-                'template' => $page->getTemplate(),
-                '_controller' => $request->attributes->get('_controller'),
-                'controller' => $page->getPageController(),
-                'controllerPath' => $page->getPageControllerPath(),
+                'page' => $page,
+                'controller' => $request->attributes->get('_controller'),
             );
+        } else {
+            $this->data['page'] = null;
         }
     }
 
     public function getPage()
     {
-        return $this->data;
+        return $this->data['page'];
+    }
+
+    public function getController()
+    {
+        return $this->data['controller'];
     }
 
     public function getName()
