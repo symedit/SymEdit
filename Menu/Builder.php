86de787e778c0cd925d8486779134b76f1bff65c
Craig Blanchette <craig.blanchette@gmail.com>
2013-09-25 12:07:00 -0400

    New Admin menu through the knp_menu + new mopa bootstrap stuff

---
+++ Menu/Builder.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Isometriks\Bundle\SymEditBundle\Menu;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Isometriks\Bundle\SymEditBundle\Model\UserInterface;
+
+class Builder
+{
+    protected $container;
+    protected $factory;
+    protected $context;
+    protected $extensions;
+    
+    public function __construct(ContainerInterface $container, $extensions = array())
+    {
+        $this->factory = $container->get('knp_menu.factory');
+        $this->context = $container->get('security.context');
+        $this->container = $container;
+        $this->extensions = $extensions;
+    }
+    
+    /**
+     * @return UserInterface $user
+     */
+    protected function getUser()
+    {
+        return $this->context->getToken()->getUser();
+    }
+
+    public function adminMenu()
+    {
+        $menu = $this->factory->createItem('root', array(
+            'navbar' => true,
+        ));
+        
+        if (!$this->context->isGranted('ROLE_ADMIN')) {
+            return $menu;
+        }
+
+        $dashboard = $menu->addChild('dashboard', array('route' => 'admin_dashboard', 'label' => 'Dashboard'));
+        $content = $menu->addChild('Content', array('dropdown' => true, 'caret' => true));
+        
+        /**
+         * Pages
+         */
+        if ($this->context->isGranted('ROLE_ADMIN_PAGE')) {
+            $content->addChild('Pages', array('dropdown_header' => true));
+            $content->addChild('New Page', array('route' => 'admin_page_new', 'icon' => 'edit'));
+            $content->addChild('List Pages', array('route' => 'admin_page', 'icon' => 'file'));
+        }
+        
+        /**
+         * Blog
+         */
+        if ($this->context->isGranted('ROLE_ADMIN_BLOG')) {
+            $content->addChild('Blog', array('dropdown_header' => true));
+            $content->addChild('New Post', array('route' => 'admin_blog_new', 'icon' => 'edit'));
+            $content->addChild('List Posts', array('route' => 'admin_blog', 'icon' => 'th-list'));
+            $content->addChild('List Categories', array('route' => 'admin_blog_category', 'icon' => 'tags'));
+        }
+        
+        /**
+         * Images
+         */
+        if ($this->context->isGranted('ROLE_ADMIN_IMAGE')) {
+            $content->addChild('Images', array('dropdown_header' => true));
+            $content->addChild('Upload Image', array('route' => 'admin_image_new', 'icon' => 'upload'));
+            $content->addChild('View Images', array('route' => 'admin_image', 'icon' => 'picture'));
+            $content->addChild('Sliders', array('route' => 'admin_image_slider', 'icon' => 'film'));
+        }
+        
+        
+        /**
+         * Widgets
+         */
+        if ($this->context->isGranted('ROLE_ADMIN_WIDGET')) {
+            $structure = $menu->addChild('Structure', array('dropdown' => true, 'caret' => true));
+            $structure->addChild('Widgets', array('dropdown_header' => true));
+            $structure->addChild('New Widget', array('route' => 'admin_widget_new', 'icon' => 'edit'));
+            $structure->addChild('List Widgets', array('route' => 'admin_widget', 'icon' => 'tasks'));
+        }
+        
+        /**
+         * Site
+         */
+        $settings = $this->context->isGranted('ROLE_ADMIN_SETTING');
+        $users = $this->context->isGranted('ROLE_ADMIN_USER');
+        $stylizer = $this->context->isGranted('ROLE_ADMIN_STYLIZER') && $this->container->has('isometriks_stylizer.stylizer');
+        
+        if ($settings || $users || $stylizer) {
+            $site = $menu->addChild('Site', array('dropdown' => true, 'caret' => true));
+            
+            if ($settings) {
+                $site->addChild('Settings', array('route' => 'admin_setting', 'icon' => 'cogs'));
+            }
+            
+            if ($users) {
+                $site->addChild('Users', array('route' => 'admin_user', 'icon' => 'group'));
+            }
+            
+            if ($stylizer) {
+                $site->addChild('Stylizer', array('route' => 'admin_stylizer', 'icon' => 'magic'));
+            }
+        }
+        
+        
+        /**
+         * Extensions
+         */
+        $validExtensions = array_filter($this->extensions, function($extension) {
+            return !isset($extension['role']) || $this->context->isGranted($extension['role']);
+        });
+        
+        if (count($validExtensions) > 0) {
+            $extensions = $menu->addChild('Extensions', array(
+                'dropdown' => true,
+                'caret' => true,
+            ));
+        
+            $index = 0;
+            foreach ($this->extensions as $extension) {
+                $extensions->addChild('extension_'.$index++, $extension);
+            }
+        }
+        
+        return $menu;
+    }
+    
+    public function adminUserMenu()
+    {
+        $menu = $this->factory->createItem('root', array(
+            'navbar' => true,
+            'push_right' => true,
+        ));
+        
+        $user = $this->getUser();
+        
+        $userMenu = $menu->addChild('User Menu', array(
+            'dropdown' => true,
+            'caret' => true,
+            'label' => $user->getProfile()->getFullname(),
+        ));
+            
+        $userMenu->addChild('My Profile', array('route' => 'fos_user_profile_show', 'icon' => 'user'));
+        $userMenu->addChild('Change Password', array('route' => 'fos_user_change_password', 'icon' => 'lock'));
+        $userMenu->addChild('Logout', array('route' => 'fos_user_security_logout', 'icon' => 'off'));
+        
+        return $menu;
+    }
+}
