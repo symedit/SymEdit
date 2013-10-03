f3e52f5b9a4bf274dc2dc970a73c40df34d1169d
Craig Blanchette <craig.blanchette@gmail.com>
2013-10-03 17:26:22 -0400

    unused namespace

--- Form/PageType.php
+++ Form/PageType.php
@@ -5,7 +5,6 @@ namespace Isometriks\Bundle\SymEditBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Doctrine\ORM\EntityRepository;
 use Isometriks\Bundle\SymEditBundle\Model\PageManagerInterface;
 
 class PageType extends AbstractType 
