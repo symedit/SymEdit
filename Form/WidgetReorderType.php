c9e4867d60a6d30dcf48a42f9098ab5076580d25
Craig Blanchette <craig.blanchette@gmail.com>
2013-10-09 12:24:25 -0400

    Widget reorder form

---
+++ Form/WidgetReorderType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Isometriks\Bundle\SymEditBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class WidgetReorderType extends AbstractType 
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if(!$options['render']){
+            $builder
+                    ->add('pair', 'collection', array(
+                        'type' => 'integer',
+                        'property_path' => '[pair]', 
+                        'allow_add' => true, 
+                    ));
+        }
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'render' => false, 
+        ));
+    }
+
+    public function getName()
+    {
+        return 'isometriks_widget_reorder';
+    }
+}
