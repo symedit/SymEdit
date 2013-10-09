efab9d0cea5056444ed1dbd86c574f7293f9cb3b
Craig Blanchette <craig.blanchette@gmail.com>
2013-10-09 12:22:16 -0400

    Added submenu ability

--- Menu/Provider/TreeMenuProvider.php
+++ Menu/Provider/TreeMenuProvider.php
@@ -4,6 +4,7 @@ namespace Isometriks\Bundle\SymEditBundle\Menu\Provider;
 
 use Knp\Menu\Provider\MenuProviderInterface;
 use Isometriks\Bundle\SymEditBundle\Model\PageManagerInterface;
+use Isometriks\Bundle\SymEditBundle\Model\PageInterface;
 use Knp\Menu\FactoryInterface;
 use Knp\Menu\MenuItem;
 
@@ -23,12 +24,43 @@ class TreeMenuProvider implements MenuProviderInterface
 
     public function get($name, array $options = array())
     {
-        $root = $this->pageManager->findRoot();
         $menu = $this->factory->createItem('root');
-
-        $this->populateChildren($menu, $root->getChildren());
+        $this->populateChildren($menu, $this->getRoot($options));
 
         return $menu;
+    }
+    
+    /**
+     * Gets root elements from options
+     * 
+     * @return PageInterface 
+     */
+    protected function getRoot(array $options)
+    {
+        if (isset($options['root']) && $options['root'] instanceof PageInterface) {
+            $root = $options['root'];
+        } else {
+            $root = $this->pageManager->findRoot();
+        }
+        
+        /**
+         * If you provide a 'level' it will move up the tree until it matches
+         * your desired level.
+         */
+        if (isset($options['level'])) {
+            $level = $options['level'];
+            
+            if ($level > ($root->getLevel()+1)) {
+                throw new \Exception(sprintf('Cannot get a level (%d) higher than this page\'s children (%d), '
+                                           . 'it is impossible to tell the path', $level, ($root->getLevel()+1)));
+            }
+            
+            while ($root->getLevel() >= $level && $root->getLevel() > 0) {
+                $root = $root->getParent();
+            }
+        }
+        
+        return $root;
     }
 
     /**
@@ -36,9 +68,9 @@ class TreeMenuProvider implements MenuProviderInterface
      * @param \Knp\Menu\NodeInterface $node
      * @param \Isometriks\Bundle\SymEditBundle\Model\PageInterface $children
      */
-    protected function populateChildren(MenuItem $menu, $children)
+    protected function populateChildren(MenuItem $menu, PageInterface $parent)
     {
-        foreach($children as $child) {
+        foreach($parent as $child) {
             if(!$child->getDisplay()) {
                 continue;
             }
@@ -49,10 +81,9 @@ class TreeMenuProvider implements MenuProviderInterface
 
             // Store Page ID
             $item->setExtra('_page_id', $child->getId());
-
             $menu->addChild($item);
 
-            $this->populateChildren($item, $child->getChildren());
+            $this->populateChildren($item, $child);
         }
     }
 
