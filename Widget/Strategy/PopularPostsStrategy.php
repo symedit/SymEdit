f6f7d1a426c7ac2b58f81fb494f89a08091dd7ef
Craig Blanchette <craig.blanchette@gmail.com>
2013-11-19 12:22:23 -0500

    Added popular posts widget

---
+++ Widget/Strategy/PopularPostsStrategy.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Isometriks\Bundle\SymEditBundle\Widget\Strategy;
+
+use Isometriks\Bundle\SymEditBundle\Model\PageInterface;
+use Isometriks\Bundle\SymEditBundle\Model\PostManagerInterface;
+use Isometriks\Bundle\SymEditBundle\Model\WidgetInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Range;
+
+class PopularPostsStrategy extends AbstractWidgetStrategy
+{
+    private $postManager;
+
+    public function __construct(PostManagerInterface $postManager)
+    {
+        $this->postManager = $postManager;
+    }
+
+    public function execute(WidgetInterface $widget, PageInterface $page = null)
+    {
+        $posts = $this->postManager->findPopular($widget->getOption('max'));
+
+        return $this->render('@SymEdit/Widget/blog-popular-posts.html.twig', array(
+            'posts' => $posts,
+        ));
+    }
+
+    public function buildForm(FormBuilderInterface $builder)
+    {
+        $builder
+            ->add('max', 'integer', array(
+                'label' => 'Max Posts',
+                'help_block' => 'Maximum Posts to display in Widget',
+                'constraints' => array(
+                    new Range(array(
+                        'min' => 1,
+                        'minMessage' => 'Minimum posts is 1, if you want less disable the widget.',
+                    )),
+                ),
+            ));
+    }
+
+    public function setDefaultOptions(WidgetInterface $widget)
+    {
+        $widget->setOptions(array(
+            'max' => 3,
+        ));
+    }
+
+    public function getName()
+    {
+        return 'blog_popular_posts';
+    }
+
+    public function getDescription()
+    {
+        return 'Popular Posts';
+    }
+}
