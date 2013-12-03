ccc79efc6095b5f82113b0c97674fffdc4035003
Craig Blanchette <craig.blanchette@gmail.com>
2013-12-03 14:10:01 -0500

    Take og:description from the meta tag not the actual description field.

--- EventListener/Seo/OpenGraphCalculator.php
+++ EventListener/Seo/OpenGraphCalculator.php
@@ -6,31 +6,31 @@ use Isometriks\Bundle\SeoBundle\Event\SeoEvent;
 use Isometriks\Bundle\SeoBundle\Model\SeoCalculatorInterface;
 
 class OpenGraphCalculator implements SeoCalculatorInterface
-{    
+{
     public function calculateSeo(SeoEvent $event)
     {
         $seo = $event->getSeo();
         $request = $event->getRequest();
-        
+
         /**
          * Add current location
          */
         if ($request !== null) {
             $seo->addMetaProperty('og:url', $request->getUri());
         }
-        
+
         /**
          * OpenGraph Title
          */
         if (!$seo->hasMeta('property', 'og:title')) {
             $seo->addMetaProperty('og:title', $seo->getTitle());
         }
-        
+
         /**
          * OpenGraph Description
          */
-        if (!$seo->hasMeta('property', 'og:description')) {
-            $seo->addMetaProperty('og:description', $seo->getDescription());
+        if (!$seo->hasMeta('property', 'og:description') && $seo->hasMeta('name', 'description')) {
+            $seo->addMetaProperty('og:description', $seo->getMetas('name', 'description'));
         }
     }
 }
