9dd969b145e610d4ffcd5832e465907128464b31
Craig Blanchette <craig.blanchette@gmail.com>
2013-12-03 13:49:04 -0500

    Added some more static methods

--- Util/SeoTools.php
+++ Util/SeoTools.php
@@ -8,9 +8,39 @@ class SeoTools
     {
         // idk
     }
-    
+
+    /**
+     * Removes XSS issues mostly, html entities and strip_tags
+     *
+     * @param string $string
+     * @return string
+     */
     public static function normalize($string)
     {
         return htmlentities(strip_tags($string));
+    }
+
+    /**
+     * Removes outer whitespace, and removes double spaces / newlines
+     *
+     * @param string $string
+     */
+    public static function minify($string)
+    {
+        return trim(preg_replace('#\s+#', ' ', $string));
+    }
+
+    /**
+     * Normalize then minify
+     *
+     * @param string $string
+     * @return string
+     */
+    public static function plain($string, $length = null)
+    {
+        $normal = self::normalize($string);
+        $minify = self::minify($normal);
+
+        return $length === null ? $minify : substr($minify, 0, $length);
     }
 }
