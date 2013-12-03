55d63cb84ab04d5fd7a44ef7b85d12729d4a5ac9
Craig Blanchette <craig.blanchette@gmail.com>
2013-12-03 13:48:54 -0500

    Added some of the public properties to interface

--- Model/SeoInterface.php
+++ Model/SeoInterface.php
@@ -3,5 +3,16 @@
 namespace Isometriks\Bundle\SeoBundle\Model;
 
 interface SeoInterface extends SeoAbleInterface
-{    
+{
+    /**
+     * Adds a meta tag
+     */
+    public function addMeta($type, $key, $content);
+
+    /**
+     * Check if meta exists
+     *
+     * @return boolean
+     */
+    public function hasMeta($type, $key = null);
 }
