88af7139a95ca92f405f0dbdfc3c8714abdb97aa
Craig Blanchette <craig.blanchette@gmail.com>
2013-12-12 15:50:11 -0500

    Made it so the social one doesn't disappear

--- Resources/public/js/snippets/flash-hide.js
+++ Resources/public/js/snippets/flash-hide.js
@@ -1,8 +1,6 @@
 $(function(){
     window.setTimeout(function(){
-        var $flash = $('#flash'); 
-        $flash.find('.message').slideUp(400, function(){
-            $flash.hide(); 
-        }); 
-    }, 5000); 
-}); 
+        var $flash = $('#flash');
+        $flash.find('.message').slideUp(400);
+    }, 5000);
+});
