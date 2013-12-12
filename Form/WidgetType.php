cc99e8a032e9c68d58ffbda2af10ec3f1d5c4461
Craig Blanchette <craig.blanchette@gmail.com>
2013-12-12 12:24:26 -0500

    Fixed tabs class

--- Form/WidgetType.php
+++ Form/WidgetType.php
@@ -5,87 +5,91 @@ namespace Isometriks\Bundle\SymEditBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Isometriks\Bundle\SymEditBundle\Form\DataTransformer\WidgetAssociationTransformer; 
-use Isometriks\Bundle\SymEditBundle\Model\Widget; 
+use Isometriks\Bundle\SymEditBundle\Form\DataTransformer\WidgetAssociationTransformer;
+use Isometriks\Bundle\SymEditBundle\Model\Widget;
 
 class WidgetType extends AbstractType
-{    
+{
     protected $widgetClass;
     protected $widgetAreaClass;
-    
+
     public function __construct($widgetClass, $widgetAreaClass)
     {
         $this->widgetClass = $widgetClass;
         $this->widgetAreaClass = $widgetAreaClass;
     }
 
-    
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $transformer = new WidgetAssociationTransformer();
-        
+
         $basic = $builder->create('basic', 'tab', array(
-            'label' => 'Basic', 
+            'label' => 'Basic',
             'icon' => 'info-sign',
-            'inherit_data' => true, 
-            'data_class' => $this->widgetClass, 
+            'inherit_data' => true,
+            'data_class' => $this->widgetClass,
         ))
             ->add('title')
             ->add('name', 'text', array(
-                'help_block' => 'Numbers, letters, underscores, or hyphens.', 
+                'help_block' => 'Numbers, letters, underscores, or hyphens.',
             ))
             ->add('area', 'entity', array(
-                'property' => 'area', 
-                'class' => $this->widgetAreaClass, 
+                'property' => 'area',
+                'class' => $this->widgetAreaClass,
             ))
             ->add('visibility', 'choice', array(
-                'label' => 'Visibility', 
+                'label' => 'Visibility',
                 'choices' => array(
-                    Widget::INCLUDE_ALL => 'Include on ALL Pages', 
-                    Widget::INCLUDE_ONLY => 'Include ONLY on specified', 
-                    Widget::EXCLUDE_ONLY => 'Exclude ONLY on specified', 
-                ), 
+                    Widget::INCLUDE_ALL => 'Include on ALL Pages',
+                    Widget::INCLUDE_ONLY => 'Include ONLY on specified',
+                    Widget::EXCLUDE_ONLY => 'Exclude ONLY on specified',
+                ),
             ))
             ->add(
                 $builder->create('assoc', 'textarea', array(
                     'label' => 'Associations',
-                    'required' => false, 
-                    'auto_initialize' => false, 
+                    'required' => false,
+                    'auto_initialize' => false,
                     'attr' => array(
-                        'rows' => 8, 
+                        'rows' => 8,
                     ),
                 ))->addModelTransformer($transformer)
             );
-        
+
         $builder->add($basic);
-                        
+
         /**
          * Build the config form from the strategy
          */
         $config = $builder->create('options', 'tab', array(
             'label' => 'Options',
             'icon' => 'cog',
-        )); 
-        
-        $options['strategy']->buildForm($config); 
-        
+        ));
+
+        $options['strategy']->buildForm($config);
+
         /**
          * Add to the final form if config has children
          */
         if($config->count() > 0){
-            $builder->add($config); 
+            $builder->add($config);
         }
-        
-        return $builder->getForm(); 
+
+        return $builder->getForm();
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
+        $resolver->setDefaults(array(
+            'tabs_class' => 'nav nav-pills nav-stacked',
+        ));
+
         $resolver->setRequired(array(
             'strategy',
         ));
     }
-    
+
     public function getName()
     {
         return 'symedit_widget';
