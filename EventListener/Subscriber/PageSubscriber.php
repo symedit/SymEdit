ec70a76dbf56a0f5d5037b3c36e6b9508bc6b115
Craig Blanchette <craig.blanchette@gmail.com>
2013-12-19 13:37:37 -0500

    Added page update event

--- EventListener/Subscriber/PageSubscriber.php
+++ EventListener/Subscriber/PageSubscriber.php
@@ -5,26 +5,32 @@ namespace Isometriks\Bundle\SymEditBundle\EventListener\Subscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Isometriks\Bundle\SymEditBundle\Event\Events;
 use Symfony\Component\HttpFoundation\Session\Session;
-use Isometriks\Bundle\SymEditBundle\Event\PageEvent;
+use Sylius\Bundle\ResourceBundle\Event\ResourceEvent;
 
 class PageSubscriber implements EventSubscriberInterface
 {
     protected $session;
-    
+
     public function __construct(Session $session)
     {
         $this->session = $session;
     }
-    
+
     public static function getSubscribedEvents()
     {
         return array(
             Events::PAGE_CREATED => 'pageCreated',
+            Events::PAGE_UPDATED => 'pageUpdated',
         );
     }
-    
-    public function pageCreated(PageEvent $event)
+
+    public function pageCreated(ResourceEvent $event)
     {
         $this->session->getFlashBag()->add('notice', 'admin.page.flash.created');
+    }
+
+    public function pageUpdated(ResourceEvent $event)
+    {
+        $this->session->getFlashBag()->add('notice', 'admin.page.flash.updated');
     }
 }
