9c29c55fb300fa8db92dfdf4d65ad5be38e9527e
Craig Blanchette <craig.blanchette@gmail.com>
2013-12-20 13:01:09 -0500

    Added a resource controller so we can extend ours in case sylius changes stuff we don't want.

---
+++ Controller/ResourceController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Isometriks\Bundle\SymEditBundle\Controller;
+
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController as BaseResourceController;
+
+class ResourceController extends BaseResourceController
+{
+
+}
