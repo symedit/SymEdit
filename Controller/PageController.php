2ad83f168dc688c383f518c9cc50b0a42011f061
Craig Blanchette <craig.blanchette@gmail.com>
2014-01-02 17:23:54 -0500

    Made page controller work with rest

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -2,28 +2,46 @@
 
 namespace Isometriks\Bundle\SymEditBundle\Controller;
 
+use FOS\RestBundle\Controller\FOSRestController;
 use Isometriks\Bundle\SymEditBundle\Event\Events;
-use Isometriks\Bundle\SymEditBundle\Event\PageEvent;
 use Isometriks\Bundle\SymEditBundle\Model\PageInterface;
+use Sylius\Bundle\ResourceBundle\Event\ResourceEvent;
 use Symfony\Component\HttpFoundation\Request;
 
-class PageController extends Controller
+class PageController extends FOSRestController
 {
-    public function showAction(PageInterface $_page, Request $request)
+    public function showAction(Request $request)
     {
-        $response = $this->createResponse($_page->getUpdatedAt());
+        /* @var $page PageInterface */
+        $page = $request->get('_page');
 
-        $event = new PageEvent($_page, $request);
-        $this->container->get('event_dispatcher')->dispatch(Events::PAGE_VIEW, $event);
+        /**
+         * Dispatch page view event
+         */
+        $event = new ResourceEvent($page);
+        $this->get('event_dispatcher')->dispatch(Events::PAGE_VIEW, $event);
+
+        /**
+         * Check for template
+         */
+        if (($template = $page->getTemplate()) === null) {
+            throw new \Exception('Page does not have a template, cannot render');
+        }
+
+        $view = $this
+            ->view()
+            ->setTemplateVar('Page')
+            ->setData($page)
+            ->setTemplate(sprintf('@SymEdit/Page/%s', $template));
+
+        $response = $view
+            ->getResponse()
+            ->setLastModified($page->getUpdatedAt());
 
         if ($response->isNotModified($request)) {
             return $response;
         }
 
-        if (($template = $_page->getTemplate()) === null) {
-            throw new \Exception('Page does not have a template, cannot render');
-        }
-
-        return $this->render(sprintf('@SymEdit/Page/%s', $template), array(), $response);
+        return $this->handleView($view);
     }
 }
