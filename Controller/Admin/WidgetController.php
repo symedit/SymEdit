b11217a5fcfbceaba1a204576236d04e31e983b8
Craig Blanchette <craig.blanchette@gmail.com>
2014-01-02 12:43:59 -0500

    Fixed how widget choose / new works. Fixed the createNew so we don't need the whole action.

--- Controller/Admin/WidgetController.php
+++ Controller/Admin/WidgetController.php
@@ -2,260 +2,111 @@
 
 namespace Isometriks\Bundle\SymEditBundle\Controller\Admin;
 
-use Isometriks\Bundle\SymEditBundle\Controller\Controller;
+use Isometriks\Bundle\SymEditBundle\Controller\ResourceController;
 use Isometriks\Bundle\SymEditBundle\Form\WidgetReorderType;
-use Isometriks\Bundle\SymEditBundle\Model\WidgetInterface;
-use Isometriks\Bundle\SymEditBundle\Widget\WidgetManager;
 use Isometriks\Bundle\SymEditBundle\Widget\WidgetRegistry;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use JMS\SecurityExtraBundle\Annotation\PreAuthorize;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @Route("/widget")
+ * @PreAuthorize("hasRole('ROLE_ADMIN_WIDGET')")
  */
-class WidgetController extends Controller
+class WidgetController extends ResourceController
 {
-    private $manager;
-
-    /**
-     * @Route("/", name="admin_widget")
-     * @Template()
-     */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        $manager = $this->getManager();
-        $areas = $manager->getWidgetAreas();
+        $areas = $this->get('isometriks_symedit.repository.widget_area')->findAll();
 
         $reorderForm = $this->createForm(new WidgetReorderType(), null, array(
+            'method' => 'POST',
+            'action' => $this->generateUrl('admin_widget_reorder'),
             'render' => true,
+            'attr' => array(
+                'id' => 'reorder-form',
+            )
         ));
-        
-        return array(
-            'reorder_form' => $reorderForm->createView(),
-            'areas' => $areas,
-        );
+
+        $view = $this
+            ->view()
+            ->setTemplate('@SymEdit/Admin/Widget/index.html.twig')
+            ->setData(array(
+                'form' => $reorderForm->createView(),
+                'areas' => $areas,
+            ));
+
+        return $this->handleView($view);
     }
-    
+
+    public function getForm($resource = null)
+    {
+        return $this->createForm($this->getConfiguration()->getFormType(), $resource, array(
+            'strategy' => $resource->getStrategy(),
+        ));
+    }
+
     /**
-     * @Route("/reorder", name="admin_widget_reorder")
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws type
      */
     public function reorderAction(Request $request)
     {
-
-        $reorder_form = $this->createForm(new WidgetReorderType());
-        $reorder_form->handleRequest($request);
+        $reorderForm = $this->createForm(new WidgetReorderType());
+        $reorderForm->handleRequest($request);
         $status = false;
 
-        if($reorder_form->isValid()){
+        if($reorderForm->isValid()){
             $status = true;
-            $data = $reorder_form->getData();
-            $widgetManager = $this->getManager();
+            $data = $reorderForm->getData();
+            $manager = $this->getManager();
+            $repository = $this->getRepository();
 
             foreach($data['pair'] as $id=>$order){
-                if(!$widget = $widgetManager->findWidget($id)){
+                if(!$widget = $repository->find($id)){
                     throw $this->createNotFoundException(sprintf('Sorting entity not found (%d)', $id));
                 }
 
                 $widget->setWidgetOrder($order);
-                $widgetManager->saveWidget($widget);
+                $manager->persist($widget);
             }
+
+            $manager->flush();
         }
 
-        return new JsonResponse(array(
-            'status' => $status,
-        ));
+        $view = $this
+            ->view()
+            ->setFormat('json')
+            ->setData(array(
+                'status' => $status
+            ));
+
+        return $this->handleView($view);
     }
 
-    /**
-     * @Route("/new/{strategyName}", name="admin_widget_new")
-     * @Template()
-     */
-    public function newAction($strategyName = null)
+    public function chooseAction()
     {
-        if($strategyName === null){
+        /* @var $registry WidgetRegistry */
+        $registry = $this->get('isometriks_symedit.widget.registry');
+        $strategies = $registry->getStrategies();
 
-            /* @var $registry WidgetRegistry */
-            $registry = $this->get('isometriks_symedit.widget.registry');
-            $strategies = $registry->getStrategies();
-
-            return $this->render('@IsometriksSymEdit/Admin/Widget/choose.html.twig', array(
+        $view = $this
+            ->view()
+            ->setTemplate('@SymEdit/Admin/Widget/choose.html.twig')
+            ->setTemplateVar('strategies')
+            ->setData(array(
                 'strategies' => $strategies,
             ));
 
-        } else {
-
-            $widget = $this->createWidget($strategyName);
-            $form = $this->createCreateForm($widget);
-
-            return array(
-                'entity' => $widget,
-                'form' => $form->createView(),
-            );
-        }
-    }
-    
-    private function createCreateForm(WidgetInterface $widget)
-    {
-        return $this->createForm('symedit_widget', $widget, array(
-            'strategy' => $widget->getStrategy(),
-            'action' => $this->generateUrl('admin_widget_create', array('strategyName' => $widget->getStrategyName())),
-            'method' => 'POST',
-        ));
+        return $this->handleView($view);
     }
 
-    /**
-     * @Route("/create/{strategyName}", name="admin_widget_create")
-     * @Template("@IsometriksSymEdit/Admin/Widget/new.html.twig")
-     */
-    public function createAction(Request $request, $strategyName)
+    public function createNew()
     {
-        $widget = $this->createWidget($strategyName);
-        $form = $this->createCreateForm($widget);
+        $strategyName = $this->getRequest()->get('strategyName');
 
-        $form->handleRequest($request);
-
-        if($form->isValid()){
-            $this->getManager()->saveWidget($widget);
-            $this->addFlash('notice', 'Widget created successfully.');
-
-            return $this->redirect($this->generateUrl('admin_widget_edit', array('id' => $widget->getId())));
-        }
-
-        $this->addFlash('error', 'Error Creating Widget.');
-
-        return array(
-            'entity' => $widget,
-            'form' => $form->createView(),
-        );
-    }
-
-    /**
-     * @Route("/{id}/edit", name="admin_widget_edit")
-     * @Method("GET")
-     * @Template()
-     */
-    public function editAction($id)
-    {
-        $manager = $this->getManager();
-        $widget = $manager->getWidgetBy(array('id' => $id));
-
-        if($widget === null){
-            throw new \Exception(sprintf('Could not find widget with name "%s".', $name));
-        }
-
-        $form = $this->createEditForm($widget);
-
-        return array(
-            'entity' => $widget,
-            'form' => $form->createView(),
-            'delete_form' => $this->createDeleteForm($id)->createView(),
-        );
-    }
-    
-    private function createEditForm(WidgetInterface $widget)
-    {
-        return $this->createForm('symedit_widget', $widget, array(
-            'strategy' => $widget->getStrategy(),
-            'action' => $this->generateUrl('admin_widget_update', array('id' => $widget->getId())),
-            'method' => 'PUT',
-        ));
-    }
-
-    /**
-     * @Route("/{id}/update", name="admin_widget_update")
-     * @Method("PUT")
-     * @Template("@IsometriksSymEdit/Admin/Widget/edit.html.twig")
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $manager = $this->getManager();
-        $widget = $manager->getWidgetBy(array('id' => $id));
-
-        if($widget === null){
-            throw new \Exception(sprintf('Could not find widget with name "%s".', $name));
-        }
-
-        $form = $this->createEditForm($widget);
-        $form->handleRequest($request);
-
-        if($form->isValid()){
-            $manager->saveWidget($widget);
-
-            $this->addFlash('notice', 'Widget Updated.');
-
-            return $this->redirect($this->generateUrl('admin_widget_edit', array('id' => $id)));
-        }
-
-        $this->addFlash('error', 'Error Updating Widget');
-
-        return array(
-            'entity' => $widget,
-            'form' => $form->createView(),
-        );
-    }
-
-
-    /**
-     * Deletes a Widget entity.
-     *
-     * @Route("/{id}/delete", name="admin_widget_delete")
-     * @Method("POST")
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $widgetManager = $this->getManager();
-            $widget = $widgetManager->findWidget($id);
-
-            if (!$widget) {
-                throw $this->createNotFoundException('Unable to find Widget entity.');
-            }
-
-            $widgetManager->deleteWidget($widget);
-            $this->addFlash('notice', 'Widget Deleted');
-        }
-
-        return $this->redirect($this->generateUrl('admin_widget'));
-    }
-
-    /**
-     * @return WidgetManager $manager
-     */
-    private function getManager()
-    {
-        if($this->manager === null){
-            $this->manager = $this->get('isometriks_symedit.widget.manager');
-        }
-
-        return $this->manager;
-    }
-
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder(array('id' => $id))
-            ->add('id', 'hidden')
-            ->getForm()
-        ;
-    }
-
-    /**
-     * @param string $strategyName
-     * @return WidgetInterface
-     */
-    private function createWidget($strategyName)
-    {
-        /**
-         * Try to create a new widget, you can type these in the address
-         * bar so don't let Symfony crash, just throw a 404.
-         */
         try {
-            $widget = $this->getManager()->createWidget($strategyName);
+            $widget = $this->getRepository()->createNew($strategyName);
         } catch(\Exception $e) {
             throw $this->createNotFoundException(sprintf('Widget with strategy name "%s" does not exist', $strategyName));
         }
