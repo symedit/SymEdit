2513914e874bf8710c4d5a68cae6439610b87431
Craig Blanchette <craig.blanchette@gmail.com>
2014-01-04 21:24:57 -0500

    Make my own resource extension

--- DependencyInjection/IsometriksSymEditExtension.php
+++ DependencyInjection/IsometriksSymEditExtension.php
@@ -2,10 +2,9 @@
 
 namespace Isometriks\Bundle\SymEditBundle\DependencyInjection;
 
-use Sylius\Bundle\ResourceBundle\DependencyInjection\SyliusResourceExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class IsometriksSymEditExtension extends SyliusResourceExtension
+class IsometriksSymEditExtension extends SymEditResourceExtension
 {
     protected $configFiles = array(
         'services', 'user', 'widget', 'routing',
@@ -28,37 +27,6 @@ class IsometriksSymEditExtension extends SyliusResourceExtension
         $container->setParameter('isometriks_symedit.model_manager_name', $config['model_manager_name']);
         $container->setParameter('isometriks_symedit.extensions.routes', $config['extensions']);
         $container->setParameter('isometriks_symedit.admin_dir', $config['admin_dir']);
-    }
-
-    /**
-     * Remap class parameters.
-     *
-     * @param array            $classes
-     * @param ContainerBuilder $container
-     */
-    protected function mapClassParameters(array $classes, ContainerBuilder $container)
-    {
-        foreach ($classes as $model => $serviceClasses) {
-            foreach ($serviceClasses as $service => $class) {
-                $container->setParameter(sprintf('isometriks_symedit.%s.%s.class', $service === 'form' ? 'form.type' : $service, $model), $class);
-            }
-        }
-    }
-
-    /**
-     * Maps parameters to their equivalent path in their arrays
-     *
-     * @param ContainerBuilder $container
-     * @param string $path
-     * @param array $config
-     */
-    private function remapParameters(ContainerBuilder $container, $path, array $config)
-    {
-        $prefix = rtrim($this->getAlias().'.'.$path, '.');
-
-        foreach ($config as $key => $value) {
-            $container->setParameter(sprintf('%s.%s', $prefix, $key), $value);
-        }
     }
 
     public function getAlias()
