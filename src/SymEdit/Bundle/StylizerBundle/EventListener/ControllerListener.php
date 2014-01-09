573178ae8b0a87f40450009a6362d26d0d5a425c
Craig Blanchette <craig.blanchette@gmail.com>
2014-01-09 17:27:37 -0500

    Unused classes

--- src/SymEdit/Bundle/StylizerBundle/EventListener/ControllerListener.php
+++ src/SymEdit/Bundle/StylizerBundle/EventListener/ControllerListener.php
@@ -12,39 +12,36 @@
 namespace SymEdit\Bundle\StylizerBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
-use Symfony\Component\HttpKernel\HttpKernel;
-use SymEdit\Bundle\StylizerBundle\Model\Stylizer; 
-
-use Symfony\Component\HttpKernel\Event\GetResponseEvent; 
+use SymEdit\Bundle\StylizerBundle\Model\Stylizer;
 
 /**
- * This listener checks requests for controllers. If it finds the @PageController annotation, 
- * and finds a corresponding Page, then it will inject it into the controller for you. 
+ * This listener checks requests for controllers. If it finds the @PageController annotation,
+ * and finds a corresponding Page, then it will inject it into the controller for you.
  */
 class ControllerListener
 {
-    private $stylizer; 
-    private $controller = 'Symfony\Bundle\AsseticBundle\Controller\AsseticController'; 
-    private $injected; 
+    private $stylizer;
+    private $controller = 'Symfony\Bundle\AsseticBundle\Controller\AsseticController';
+    private $injected;
 
     public function __construct(Stylizer $stylizer)
     {
-        $this->stylizer = $stylizer; 
-        $this->injected = false; 
+        $this->stylizer = $stylizer;
+        $this->injected = false;
     }
 
     public function onKernelController(FilterControllerEvent $event)
     {
-        $controller = $event->getController(); 
-        
+        $controller = $event->getController();
+
         if(!is_array($controller)){
-            return; 
+            return;
         }
-        
+
         $refl = new \ReflectionClass($controller[0]);
-        
+
         if(!$this->injected && $refl->getName() === $this->controller){
-            $this->stylizer->inject(); 
+            $this->stylizer->inject();
         }
     }
 }
