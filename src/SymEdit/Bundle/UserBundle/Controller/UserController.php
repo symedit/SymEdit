bb893adbaff187eab9811ced28e0303a5c58809d
Craig Blanchette <craig.blanchette@gmail.com>
2014-01-15 15:38:56 -0500

    Removed preauthorize annotation

--- src/SymEdit/Bundle/UserBundle/Controller/UserController.php
+++ src/SymEdit/Bundle/UserBundle/Controller/UserController.php
@@ -12,12 +12,9 @@
 namespace SymEdit\Bundle\UserBundle\Controller;
 
 use SymEdit\Bundle\ResourceBundle\Controller\ResourceController;
-use JMS\SecurityExtraBundle\Annotation\PreAuthorize;
 
 /**
  * User controller.
- *
- * @PreAuthorize("hasRole('ROLE_ADMIN_USER')")
  */
 class UserController extends ResourceController
 {
