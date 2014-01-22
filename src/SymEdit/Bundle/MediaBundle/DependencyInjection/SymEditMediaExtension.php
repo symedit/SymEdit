c8791ee20587fbeaeb7737775971781f6944f15e
Craig Blanchette <craig.blanchette@gmail.com>
2014-01-21 19:52:39 -0500

    Setup extension, add in form.xml

--- src/SymEdit/Bundle/MediaBundle/DependencyInjection/SymEditMediaExtension.php
+++ src/SymEdit/Bundle/MediaBundle/DependencyInjection/SymEditMediaExtension.php
@@ -3,42 +3,29 @@
 namespace SymEdit\Bundle\MediaBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use SymEdit\Bundle\ResourceBundle\DependencyInjection\SymEditResourceExtension;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
-class SymEditMediaExtension extends Extension
+class SymEditMediaExtension extends SymEditResourceExtension
 {
+    protected $configFiles = array(
+        'services', 'form',
+    );
+
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $config, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $this->configDir = __DIR__.'/../Resources/config';
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-
-        $loader->load('services.xml');
-
-        $driver = $config['db_driver'];
-        $loader->load(sprintf('driver/%s.xml', $driver));
+        list($config) = $this->configure($config, new Configuration(), $container, self::CONFIGURE_LOADER | self::CONFIGURE_DATABASE | self::CONFIGURE_PARAMETERS);
 
         $container->setParameter('symedit_media.model_manager_name', $config['model_manager_name']);
-        $container->setParameter(sprintf('%s.backend_type_%s', $this->getAlias(), $driver), true);
-
-        /**
-         * Set directories
-         */
-        $container->setParameter('symedit_media.web_root', rtrim($config['web_root'], '/'));
-        $container->setParameter('symedit_media.upload_dir', ltrim($config['upload_dir'], '/'));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getAlias()
     {
         return 'symedit_media';
