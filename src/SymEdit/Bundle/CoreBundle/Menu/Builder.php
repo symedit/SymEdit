837794f707a7074c7c81fc682c2cad6b955f18fd
Craig Blanchette <craig.blanchette@gmail.com>
2014-01-22 12:10:58 -0500

    Added routing, templates and menu items for Files

--- src/SymEdit/Bundle/CoreBundle/Menu/Builder.php
+++ src/SymEdit/Bundle/CoreBundle/Menu/Builder.php
@@ -78,6 +78,12 @@ class Builder
          */
         if ($this->context->isGranted('ROLE_ADMIN_IMAGE')) {
             $media = $menu->addChild('Media', array('dropdown' => true, 'caret' => true, 'icon' => 'picture'));
+
+            $media->addChild('Files', array('dropdown-header' => true));
+            $media->addChild('View Files', array('route' => 'admin_file', 'icon' => 'file'));
+            $media->addChild('Upload File', array('route' => 'admin_file_create', 'icon' => 'upload'));
+
+            $media->addChild('Images', array('dropdown-header' => true));
             $media->addChild('View Images', array('route' => 'admin_image', 'icon' => 'picture'));
             $media->addChild('Upload Image', array('route' => 'admin_image_create', 'icon' => 'upload'));
             $media->addChild('Sliders', array('route' => 'admin_image_slider', 'icon' => 'film'));
