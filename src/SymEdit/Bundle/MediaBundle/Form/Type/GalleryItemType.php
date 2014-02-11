d7b38bb25354693647cb6b9a2cb260ae9bca275d
Craig Blanchette <craig.blanchette@gmail.com>
2014-02-11 15:13:17 -0500

    Added ability to add, edit and remove gallery items

---
+++ src/SymEdit/Bundle/MediaBundle/Form/Type/GalleryItemType.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the SymEdit package.
+ *
+ * (c) Craig Blanchette <craig.blanchette@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymEdit\Bundle\MediaBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class GalleryItemType extends AbstractType
+{
+    protected $class;
+
+    public function __construct($class)
+    {
+        $this->class = $class;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('image', 'symedit_image', array(
+                'show_image' => true,
+            ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => $this->class,
+        ));
+    }
+
+    public function getName()
+    {
+        return 'symedit_gallery_item';
+    }
+}
