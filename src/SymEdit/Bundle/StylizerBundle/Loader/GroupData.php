da2918a699d91eb93454662ccd4fd06da48d4497
Craig Blanchette <craig.blanchette@gmail.com>
2014-03-19 14:13:12 -0400

    CS

--- src/SymEdit/Bundle/StylizerBundle/Loader/GroupData.php
+++ src/SymEdit/Bundle/StylizerBundle/Loader/GroupData.php
@@ -9,72 +9,72 @@
  * file that was distributed with this source code.
  */
 
-namespace SymEdit\Bundle\StylizerBundle\Loader; 
+namespace SymEdit\Bundle\StylizerBundle\Loader;
 
 class GroupData
 {
-    private $name; 
-    private $variables; 
-    private $label; 
-    private $extra; 
-    
+    private $name;
+    private $variables;
+    private $label;
+    private $extra;
+
     public function __construct($name)
     {
-        $this->name = $name; 
-        $this->extra = array(); 
+        $this->name = $name;
+        $this->extra = array();
     }
-    
+
     public function getName()
     {
         return $this->name;
     }
-    
+
     public function setLabel($label)
     {
-        $this->label = $label; 
+        $this->label = $label;
     }
-    
+
     public function getLabel()
     {
-        return $this->label; 
+        return $this->label;
     }
-    
+
     public function addExtra(array $extra)
     {
-        $this->extra = array_merge($this->extra, $extra); 
+        $this->extra = array_merge($this->extra, $extra);
     }
-    
+
     public function getExtra()
     {
-        return $this->extra; 
+        return $this->extra;
     }
-    
+
     public function addVariable($name, $data)
     {
         if(!is_array($data)){
-            
+
             $this->variables[$name] = array(
-                'value' => $data, 
-            ); 
-            
+                'value' => $data,
+            );
+
         } else {
-            $this->variables[$name] = $data; 
+            $this->variables[$name] = $data;
         }
     }
-    
+
     public function getVariables()
     {
-        $variables = array(); 
+        $variables = array();
 
         foreach($this->variables as $name => $data){
-            $variables[$name] = $data['value']; 
+            $variables[$name] = $data['value'];
         }
-        
-        return $variables; 
+
+        return $variables;
     }
-    
+
     public function getVariableConfig()
     {
-        return $this->variables; 
+        return $this->variables;
     }
 }
