019059f642d6f53c7e328f5ed151895222f4cd5e
Craig Blanchette <craig.blanchette@gmail.com>
2014-05-13 16:33:27 -0400

    Moved the Contact / Page types to the type subdirectory and refactored the namespace incldues

---
+++ src/SymEdit/Bundle/CoreBundle/Form/Type/ContactType.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the SymEdit package.
+ *
+ * (c) Craig Blanchette <craig.blanchette@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymEdit\Bundle\CoreBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', array(
+                'horizontal' => false,
+                'attr' => array(
+                    'placeholder' => 'Name',
+                ),
+                'constraints' => array(
+                    new NotBlank(),
+                ),
+            ))
+            ->add('email', 'email', array(
+                'horizontal' => false,
+                'required' => false,
+                'attr' => array(
+                    'placeholder' => 'Email',
+                ),
+            ))
+            ->add('phone', 'text', array(
+                'horizontal' => false,
+                'label' => 'Phone',
+                'attr' => array(
+                    'placeholder' => 'Phone',
+                ),
+                'constraints' => array(
+                    new NotBlank(),
+                ),
+            ))
+            ->add('message', 'textarea', array(
+                'horizontal' => false,
+                'label' => 'Message',
+                'attr' => array(
+                    'rows' => 5,
+                    'placeholder' => 'Your Message',
+                ),
+                'constraints' => array(
+                    new NotBlank(),
+                ),
+            ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'timed_spam' => true,
+            'honeypot' => true,
+        ));
+    }
+
+    public function getName()
+    {
+        return 'symedit_contact';
+    }
+}
