ca2e26a430b503d9f2098c3f7394b024e75b058c
Craig Blanchette <craig.blanchette@gmail.com>
2014-05-15 17:44:25 -0400

    Header fix

--- src/SymEdit/Bundle/UserBundle/Doctrine/ORM/UserRepository.php
+++ src/SymEdit/Bundle/UserBundle/Doctrine/ORM/UserRepository.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SymEdit package.
+ *
+ * (c) Craig Blanchette <craig.blanchette@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SymEdit\Bundle\UserBundle\Doctrine\ORM;
 
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
