6efebfc0b5b794ab93db7b66196060775ad565c7
Craig Blanchette <craig.blanchette@gmail.com>
2014-05-22 10:59:05 -0400

    Cleanup some notes and add composer information

--- src/SymEdit/Bundle/ShortcodeBundle/DependencyInjection/Configuration.php
+++ src/SymEdit/Bundle/ShortcodeBundle/DependencyInjection/Configuration.php
@@ -37,47 +37,6 @@ class Configuration implements ConfigurationInterface
             ->end()
         ;
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
-        /**
-         * symedit_shortcode:
-         *     options:
-         *          icon: fontawesome4     // symedit_shortcode.options.icon
-         *          bootstrap:
-         *              default_size: md
-         *
-         * Settings:
-         *
-         * shortcode:
-         *     settings:
-         *         bootstrap:
-         *             type: shortcode_bootstrap_settings
-         *
-         * BootstrapSettingsType
-         * {
-         *     public function buildForm($builder)
-         *     {
-         *         $builder->add('default_size', 'choice', array('choices' => array('xs', 'sm', 'md', 'lg'));
-         *         $builder->add('icon', 'choice', array(
-         *             '<i class="icon-%s"></i>' => 'FontAwesome 3.x',
-         *             ... etc..
-         *         ));
-         *     }
-         *
-         *     extend: json_settings
-         * }
-         *
-         * ....Shortode ($data)
-         * {
-         *     return sprintf($this->options->pattern, ...);
-         * }
-         *
-         * The json_settings would have a data transformer. You could only ever get settings('group.setting') then you'd have to do $setting['default_size'] or whatever.
-         * Shit. You'd have to deserialize the settings when you loaded them... Or
-         */
-
         return $treeBuilder;
     }
 }
