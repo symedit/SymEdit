d2c98141ac7ff96c6fe3b6765056cb16d49f5e08
Craig Blanchette <craig.blanchette@gmail.com>
2014-05-22 10:50:32 -0400

    Headers / CS

--- src/SymEdit/Bundle/ShortcodeBundle/Shortcode/IconShortcode.php
+++ src/SymEdit/Bundle/ShortcodeBundle/Shortcode/IconShortcode.php
@@ -1,11 +1,20 @@
 <?php
 
+/*
+ * This file is part of the SymEdit package.
+ *
+ * (c) Craig Blanchette <craig.blanchette@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SymEdit\Bundle\ShortcodeBundle\Shortcode;
 
 /**
  * Renders icons. Icon Pattern like: "<i class="fa fa-%s"></i>" or "<i class="icon-%s"></i>"
  */
-class IconShortCode extends AbstractShortcode
+class IconShortcode extends AbstractShortcode
 {
     protected static $patterns = array(
         'glyphicon'     => '<span class="glyphicon glyphicon-%s"></span>',
--- src/SymEdit/Bundle/ShortcodeBundle/Twig/Extension/ShortcodeExtension.php
+++ src/SymEdit/Bundle/ShortcodeBundle/Twig/Extension/ShortcodeExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SymEdit package.
+ *
+ * (c) Craig Blanchette <craig.blanchette@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SymEdit\Bundle\ShortcodeBundle\Twig\Extension;
 
 use SymEdit\Bundle\ShortcodeBundle\Renderer\ShortcodeRendererInterface;
