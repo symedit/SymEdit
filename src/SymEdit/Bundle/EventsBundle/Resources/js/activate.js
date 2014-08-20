efc801dd3c39cdb495eba50bdfe22130b01bee97
Craig Blanchette <craig.blanchette@gmail.com>
2014-08-20 14:27:50 -0400

    Datepicker assets

---
+++ src/SymEdit/Bundle/EventsBundle/Resources/js/activate.js
@@ -0,0 +1,10 @@
+jQuery(function($) {
+    $('.datetimepicker').datetimepicker({
+        icons: {
+            time: 'icon-clock-o',
+            date: 'icon-calendar',
+            up: 'icon-arrow-up',
+            down: 'icon-arrow-down'
+        }
+    });
+});
