c5cae55299f7ed72ea265569c47de8153f60b297
Craig Blanchette <craig.blanchette@gmail.com>
2014-10-06 15:22:16 -0400

    Added a test for the slug namer

---
+++ src/SymEdit/Bundle/MediaBundle/Tests/Namer/SlugNamerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the SymEdit package.
+ *
+ * (c) Craig Blanchette <craig.blanchette@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymEdit\Bundle\MediaBundle\Tests\Namer;
+
+use SymEdit\Bundle\MediaBundle\Namer\SlugNamer;
+use SymEdit\Bundle\MediaBundle\Tests\TestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class SlugNamerTest extends TestCase
+{
+    protected $namer;
+    protected $file;
+
+    public function setUp()
+    {
+        $this->namer = new SlugNamer();
+        $this->file = tempnam(sys_get_temp_dir(), 'media_test');
+    }
+
+    public function tearDown()
+    {
+        unlink($this->file);
+    }
+
+    /**
+     * @dataProvider slugProvider
+     */
+    public function testSlugNamer($name, $expected)
+    {
+        $file = new UploadedFile($this->file, $name);
+        $slug = $this->namer->getName($file);
+
+        $this->assertEquals($expected, $slug);
+    }
+
+    public function slugProvider()
+    {
+        return array(
+            array('basic', 'basic'),
+            array('some space', 'some-space'),
+            array('mañana', 'manana'),
+            array('uPPer CaSE', 'upper-case'),
+        );
+    }
+}
