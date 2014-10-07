06f01b0717cad6ea4cf57452be4133471e3e9c48
Craig Blanchette <craig.blanchette@gmail.com>
2014-10-07 12:43:17 -0400

    Added permissions to widget menu

--- src/SymEdit/Bridge/Widget/Menu/Builder.php
+++ src/SymEdit/Bridge/Widget/Menu/Builder.php
@@ -10,9 +10,10 @@ class Builder implements MenuBuilderInterface
     public function buildMenu(MenuInterface $menu, array $options)
     {
         $structure = $menu->getRootNode()->getChild('structure');
-
-        $structure->addChild('Widgets', array('dropdown-header' => true));
-        $structure->addChild('New Widget', array('route' => 'admin_widget_choose', 'icon' => 'edit'));
-        $structure->addChild('List Widgets', array('route' => 'admin_widget', 'icon' => 'tasks'));
+        $widgetExtras = array('is_granted' => 'ROLE_ADMIN_WIDGET');
+        
+        $structure->addChild('Widgets', array('dropdown-header' => true, 'extras' => $widgetExtras));
+        $structure->addChild('New Widget', array('route' => 'admin_widget_choose', 'icon' => 'edit', 'extras' => $widgetExtras));
+        $structure->addChild('List Widgets', array('route' => 'admin_widget', 'icon' => 'tasks', 'extras' => $widgetExtras));
     }
 }
