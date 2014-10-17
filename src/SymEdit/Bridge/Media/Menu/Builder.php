95d105a746bd349515eaefd83afc7f6c888a8a0b
Craig Blanchette <craig.blanchette@gmail.com>
2014-10-17 11:03:55 -0400

    Headers

--- src/SymEdit/Bridge/Media/Menu/Builder.php
+++ src/SymEdit/Bridge/Media/Menu/Builder.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SymEdit package.
+ *
+ * (c) Craig Blanchette <craig.blanchette@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SymEdit\Bridge\Media\Menu;
 
 use SymEdit\Bundle\MenuBundle\Model\MenuBuilderInterface;
@@ -11,7 +20,7 @@ class Builder implements MenuBuilderInterface
     {
         $media = $menu->getRootNode()->getChild('media');
         $imageExtras = array('is_granted' => 'ROLE_ADMIN_IMAGE');
-        
+
         $media->addChild('Images', array('dropdown-header' => true, 'extras' => $imageExtras));
         $media->addChild('View Images', array('route' => 'admin_image', 'icon' => 'picture', 'extras' => $imageExtras));
         $media->addChild('Upload Image', array('route' => 'admin_image_create', 'icon' => 'upload', 'extras' => $imageExtras));
--- src/SymEdit/Bridge/Widget/Menu/Builder.php
+++ src/SymEdit/Bridge/Widget/Menu/Builder.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SymEdit package.
+ *
+ * (c) Craig Blanchette <craig.blanchette@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SymEdit\Bridge\Widget\Menu;
 
 use SymEdit\Bundle\MenuBundle\Model\MenuBuilderInterface;
@@ -11,7 +20,7 @@ class Builder implements MenuBuilderInterface
     {
         $structure = $menu->getRootNode()->getChild('structure');
         $widgetExtras = array('is_granted' => 'ROLE_ADMIN_WIDGET');
-        
+
         $structure->addChild('Widgets', array('dropdown-header' => true, 'extras' => $widgetExtras));
         $structure->addChild('New Widget', array('route' => 'admin_widget_choose', 'icon' => 'edit', 'extras' => $widgetExtras));
         $structure->addChild('List Widgets', array('route' => 'admin_widget', 'icon' => 'tasks', 'extras' => $widgetExtras));
