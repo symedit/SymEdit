e3bbad04a9d2239ab7e1dad67fb6e93eb02731e3
Craig Blanchette <craig.blanchette@gmail.com>
2014-10-21 15:14:53 -0400

    Added contact subscriber test

---
+++ src/SymEdit/Bundle/CoreBundle/Tests/EventListener/Subscriber/ContactSubscriberTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SymEdit\Bundle\CoreBundle\Tests\EventListener\Subscriber;
+
+use SymEdit\Bundle\CoreBundle\Event\FormEvent;
+use SymEdit\Bundle\CoreBundle\EventListener\Subscriber\ContactSubscriber;
+use SymEdit\Bundle\CoreBundle\Tests\TestCase;
+
+class ContactSubscriberTest extends TestCase
+{
+    public function testEmailSend()
+    {
+        $mailer = $this->getMock('SymEdit\Bundle\CoreBundle\Util\SymEditMailerInterface');
+        $mailer->expects($this->once())
+               ->method('sendAdmin')
+               ->with(
+                   $this->equalTo('@SymEdit/Contact/contact.html.twig'),
+                   $this->equalTo(array(
+                       'Form' => array(
+                           'email' => 'foo@bar.com',
+                       ),
+                   )));
+
+        $subscriber = new ContactSubscriber($mailer);
+        $request = $this->getMock('Symfony\Component\HttpFoundation\Request');
+
+        $form = $this->getMock('Symfony\Component\Form\FormInterface');
+        $form->method('getData')
+             ->will($this->returnValue(array(
+                 'email' => 'foo@bar.com',
+             )));
+
+        $event = new FormEvent($form, $request);
+
+        $subscriber->sendAdminEmail($event);
+    }
+}
