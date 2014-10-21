cb2b1154d013479251c3e1d3786e8592cbd95745
Craig Blanchette <craig.blanchette@gmail.com>
2014-10-21 10:56:28 -0400

    Test more pages in the functional test

--- src/SymEdit/Bundle/CoreBundle/Tests/Controller/PageControllerTest.php
+++ src/SymEdit/Bundle/CoreBundle/Tests/Controller/PageControllerTest.php
@@ -6,11 +6,27 @@ use SymEdit\Bundle\CoreBundle\Tests\WebTestCase;
 
 class PageControllerTest extends WebTestCase
 {
-    public function testHomepage()
+    /**
+     * @DataProvider fixtureUrlProvider
+     */
+    public function testPages($url)
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', $url);
 
         $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function fixtureUrlProvider()
+    {
+        return array(
+            array('/'),                         // Home
+            array('/about'),                    // About
+            array('/blog'),                     // Blog
+            array('/blog/hello-world'),         //  - Hello World
+            array('/blog/category/general'),    //  - Category: General
+            array('/login'),                    // Login Page
+            array('/sym-admin'),                // Admin Page
+        );
     }
 }
