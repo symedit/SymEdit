31b462179face164ffa934479bead3b1f90048b9
Craig Blanchette <craig.blanchette@gmail.com>
2014-10-29 12:47:30 -0400

    remove security

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,13 +3,14 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
+/*
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
+}*/
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
