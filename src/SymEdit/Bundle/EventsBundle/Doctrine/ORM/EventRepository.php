2a74f643750a6161d5a726ed4caab0b6ed444bce
Craig Blanchette <craig.blanchette@gmail.com>
2014-11-13 15:00:42 -0500

    Add in event repository so we don't show past events

---
+++ src/SymEdit/Bundle/EventsBundle/Doctrine/ORM/EventRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SymEdit\Bundle\EventsBundle\Doctrine\ORM;
+
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+
+class EventRepository extends EntityRepository
+{
+    public function getUpcoming()
+    {
+        $qb = $this->getQueryBuilder()
+                   ->where('o.eventEnd < :now')
+                   ->setParameter('now', new \DateTime());
+
+        return $this->getPaginator($qb);
+    }
+
+
+    public function getQueryBuilder()
+    {
+        return parent::getQueryBuilder()
+                ->orderBy(sprintf('%s.eventStart', $this->getAlias()), 'DESC');
+    }
+}
