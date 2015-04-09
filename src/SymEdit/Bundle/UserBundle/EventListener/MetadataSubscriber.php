7b40f35d00e89c50f659fd5c96db11742c18010f
Craig Blanchette <craig.blanchette@gmail.com>
2015-04-09 13:51:00 -0400

    Remove mapping information from Profile and let a metadatasubscriber do it so it's easier to change out the base profile and you can still use the base mapped superclass if you want it.

---
+++ src/SymEdit/Bundle/UserBundle/EventListener/MetadataSubscriber.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the SymEdit package.
+ *
+ * (c) Craig Blanchette <craig.blanchette@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymEdit\Bundle\UserBundle\EventListener;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+class MetadataSubscriber implements EventSubscriber
+{
+    protected $profile;
+    protected $adminProfile;
+
+    public function __construct($profile, $adminProfile)
+    {
+        $this->profile = $profile;
+        $this->adminProfile = $adminProfile;
+    }
+
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        $metadata = $eventArgs->getClassMetadata();
+
+        if ($metadata->getName() === $this->profile) {
+            $this->setupProfileDiscriminator($metadata);
+        }
+    }
+
+    protected function setupProfileDiscriminator(ClassMetadata $metadata)
+    {
+        // Set inheritence type
+        $metadata->setInheritanceType(ClassMetadataInfo::INHERITANCE_TYPE_JOINED);
+
+        // Set discriminator column
+        $metadata->setDiscriminatorColumn(array(
+            'name'   => 'discr',
+            'type'   => 'string',
+            'length' => 20,
+        ));
+
+        // Set map
+        $metadata->setDiscriminatorMap(array(
+            'user' => $this->profile,
+            'admin' => $this->adminProfile,
+        ));
+    }
+
+    public function getSubscribedEvents()
+    {
+        return array(
+            'loadClassMetadata',
+        );
+    }
+}
