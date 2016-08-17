183867827520a3525b43b90618369d89347c5c82
Craig Blanchette <craig.blanchette@gmail.com>
2016-08-17 15:25:45 -0400

    Don't use container in install command

--- src/SymEdit/Bundle/CoreBundle/Command/InstallCommand.php
+++ src/SymEdit/Bundle/CoreBundle/Command/InstallCommand.php
@@ -11,13 +11,13 @@
 
 namespace SymEdit\Bundle\CoreBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class InstallCommand extends ContainerAwareCommand
+class InstallCommand extends Command
 {
     protected function configure()
     {
