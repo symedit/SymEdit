fa14161934e9a4e0191f66f347cf0fd684149731
Craig Blanchette <craig.blanchette@gmail.com>
2017-03-28 23:41:16 -0400

    Reduce decimals

--- src/SymEdit/Bundle/MediaBundle/Twig/Extension/MediaExtension.php
+++ src/SymEdit/Bundle/MediaBundle/Twig/Extension/MediaExtension.php
@@ -34,7 +34,7 @@ class MediaExtension extends \Twig_Extension
         $sizes = array('B','kB','MB','GB','TB','PB','EB','ZB','YB');
         $factor = floor((strlen($bytes) - 1) / 3);
 
-        return sprintf('%.2f%s', $bytes / pow(1024, $factor), $sizes[$factor]);
+        return sprintf('%.1f %s', $bytes / pow(1024, $factor), $sizes[$factor]);
     }
 
     public function getName()
