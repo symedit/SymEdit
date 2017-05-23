1f5eb23926f72137a6c3bca167ee2192b3658cff
Craig Blanchette <craig.blanchette@gmail.com>
2017-05-23 11:15:55 -0400

    Fix blog RSS

--- src/SymEdit/Bundle/BlogBundle/Controller/PostController.php
+++ src/SymEdit/Bundle/BlogBundle/Controller/PostController.php
@@ -12,6 +12,7 @@
 namespace SymEdit\Bundle\BlogBundle\Controller;
 
 use FOS\RestBundle\View\View;
+use Sylius\Component\Resource\ResourceActions;
 use SymEdit\Bundle\ResourceBundle\Controller\ResourceController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -62,6 +63,28 @@ class PostController extends ResourceController
                 'category' => $category,
                 'posts' => $paginator,
             ]);
+
+        return $this->viewHandler->handle($configuration, $view);
+    }
+
+    public function rssAction(Request $request)
+    {
+        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
+
+        $this->isGrantedOr403($configuration, ResourceActions::INDEX);
+        $resources = $this->resourcesCollectionProvider->get($configuration, $this->repository);
+
+        $view = View::create($resources);
+        $view
+            ->setTemplate($configuration->getTemplate(ResourceActions::INDEX . '.html'))
+            ->setTemplateVar($this->metadata->getPluralName())
+            ->setData([
+                'configuration' => $configuration,
+                'metadata' => $this->metadata,
+                'resources' => $resources,
+                $this->metadata->getPluralName() => $resources,
+            ])
+        ;
 
         return $this->viewHandler->handle($configuration, $view);
     }
